126c47ac403314abb4b0aa052bc251d4fbebdcde
kysel <jkyzlink@gmail.com>
2013-01-26 16:11:21 +0100

    loko brd + initial avr sources

---
+++ Prog soubory/Lokomotivy/AtLokomotivy/AtLokomotivy.c
@@ -0,0 +1,17 @@
+﻿/*
+ * AtLokomotivy.c
+ *
+ * Created: 26.1.2013 14:12:06
+ *  Author: jirka1
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ Prog soubory/Megy dole/Megy dole/Megy dole.c
@@ -0,0 +1,17 @@
+﻿/*
+ * Megy_dole.c
+ *
+ * Created: 26.1.2013 11:23:50
+ *  Author: jirka1
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
